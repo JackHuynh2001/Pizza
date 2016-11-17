509dbf4465aab873d2b552047b0c8908e8ac4aa7
JackHuynh2001 <9jhuynh@stpatrick.org>
2016-11-17 13:55:20 -0600

    Pizza
    Finished up to toppings

---
+++ pizza.js
@@ -0,0 +1,32 @@
+class pizza{
+  //type constructor below
+  constructor(s,m,v){
+    this.size=s;
+    this.meatToppings=m;
+    this.veggieToppings=v;
+}
+
+  //type instance functions below
+sizeCost(){
+  if(this.size=s){
+    return "7.99";
+  }
+  if(this.size=m){
+  return "9.99"
+}
+  if(this.size=l){
+  return "12.99"
+}
+  if(this.size=xl){
+  return "15.99"
+}
+toppingcosts(t){
+this.topping=t;
+
+
+}
+
+  //type class functions below
+
+
+}
